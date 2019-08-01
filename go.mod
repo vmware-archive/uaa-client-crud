9c72fd90e2608233fc7cfa20e93265c8c2ee67f3
Emily Johnson <ejohnson@pivotal.io>
2019-08-01 11:03:12 -0600

    WIP
    Co-Authored-By: Aaron Meza <ameza@pivotal.io>

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/cf-platform-eng/uaa-client-crud
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.43.0 // indirect
+	code.cloudfoundry.org/credhub-cli v0.0.0-20190709211211-146ea05c785a // indirect
+	code.cloudfoundry.org/lager v2.0.0+incompatible // indirect
+	github.com/cloudfoundry-community/go-uaa v0.2.4 // indirect
+	github.com/cloudfoundry/go-socks5 v0.0.0-20180221174514-54f73bdb8a8e // indirect
+	github.com/cloudfoundry/socks5-proxy v0.2.0 // indirect
+	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70 // indirect
+	github.com/hashicorp/go-version v1.2.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/kr/pty v1.1.8 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/spf13/cobra v0.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20190729225735-1bd0cf576493 // indirect
+	golang.org/x/mobile v0.0.0-20190719004257-d2bd2a29d028 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20190801041406-cbf593c0f2f3 // indirect
+	golang.org/x/tools v0.0.0-20190731214159-1e85ed8060aa // indirect
+	google.golang.org/grpc v1.22.1 // indirect
+	honnef.co/go/tools v0.0.1-2019.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"code.cloudfoundry.org/clock"
+	"code.cloudfoundry.org/credhub-cli/credhub"
+	"code.cloudfoundry.org/credhub-cli/credhub/auth"
 	"code.cloudfoundry.org/lager"
-	client "code.cloudfoundry.org/uaa-go-client"
-	"code.cloudfoundry.org/uaa-go-client/config"
-	"code.cloudfoundry.org/uaa-go-client/schema"
 	"fmt"
+	"github.com/cloudfoundry-community/go-uaa"
 	"github.com/spf13/cobra"
 	"os"
 )
 
-type uaaCreateCmd struct {
+type uaaClientConfig struct {
 	uaaEndpoint         string
 	adminClientIdentity string
 	adminClientPwd      string
 	clientIndentity     string
 	clientPwd           string
+}
+
+type uaaClient struct {
+	config uaaClientConfig
 }
 
 func newRootCmd(args []string) *cobra.Command {
@@ -28,31 +31,29 @@ func newRootCmd(args []string) *cobra.Command {
 	flags := root.PersistentFlags()
 	//out := root.OutOrStdout()
 
-	cc := &uaaCreateCmd{
-
-	}
+	client := &uaaClient{}
 
 	uaaCreateClientCmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create a new client in UAA",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return cc.uaaCreate()
+			return client.uaaCreate()
 		},
 	}
 
-	uaaCreateClientCmd.Flags().StringVarP(&cc.uaaEndpoint, "uaa-endpoint", "e", "", "UAA Endpoint")
+	uaaCreateClientCmd.Flags().StringVarP(&client.config.uaaEndpoint, "uaa-endpoint", "e", "", "UAA Endpoint")
 	cobra.MarkFlagRequired(uaaCreateClientCmd.Flags(), "uaa-endpoint")
 
-	uaaCreateClientCmd.Flags().StringVarP(&cc.adminClientIdentity, "admin-identity", "i", "", "Admin Username")
+	uaaCreateClientCmd.Flags().StringVarP(&client.config.adminClientIdentity, "admin-identity", "i", "", "Admin Username")
 	cobra.MarkFlagRequired(uaaCreateClientCmd.Flags(), "admin-identity")
 
-	uaaCreateClientCmd.Flags().StringVarP(&cc.adminClientPwd, "admin-pwd", "p", "", "Admin Password")
+	uaaCreateClientCmd.Flags().StringVarP(&client.config.adminClientPwd, "admin-pwd", "p", "", "Admin Password")
 	cobra.MarkFlagRequired(uaaCreateClientCmd.Flags(), "admin-pwd")
 
-	uaaCreateClientCmd.Flags().StringVarP(&cc.clientIndentity, "client-identity", "c", "", "New Client Identity")
+	uaaCreateClientCmd.Flags().StringVarP(&client.config.clientIndentity, "client-identity", "c", "", "New Client Identity")
 	cobra.MarkFlagRequired(uaaCreateClientCmd.Flags(), "client-identity")
 
-	uaaCreateClientCmd.Flags().StringVarP(&cc.clientPwd, "client-pwd", "w", "", "New Client Password")
+	uaaCreateClientCmd.Flags().StringVarP(&client.config.clientPwd, "client-pwd", "w", "", "New Client Password")
 	cobra.MarkFlagRequired(uaaCreateClientCmd.Flags(), "client-pwd")
 
 	root.AddCommand(uaaCreateClientCmd)
@@ -62,50 +63,45 @@ func newRootCmd(args []string) *cobra.Command {
 	return root
 }
 
-func (cc *uaaCreateCmd) uaaCreate() error {
-	//
+func (cc *uaaClient) uaaCreate() error {
+
 	fmt.Print("the endpoint is ")
-	fmt.Println(cc.uaaEndpoint)
-
-	cfg := &config.Config{
-		ClientName:       cc.adminClientIdentity,
-		ClientSecret:     cc.adminClientPwd,
-		UaaEndpoint:      cc.uaaEndpoint,
-		SkipVerification: true,
-	}
-
+	fmt.Println(cc.config.uaaEndpoint)
 	logger := lager.NewLogger("test")
-	clock := clock.NewClock()
 
-	uaaClient, err := client.NewClient(logger, cfg, clock)
-	if  err != nil {
-		return err
+	// construct the API, and validate it
+	api := uaa.New(cc.config.uaaEndpoint, "").WithClientCredentials(cc.config.adminClientIdentity, cc.config.adminClientPwd, uaa.JSONWebToken).WithSkipSSLValidation(true)
+	err := api.Validate()
+	if err != nil {
+		logger.Info(err.Error())
 	}
 
-	fmt.Printf("Connecting to: %s ...\n", cfg.UaaEndpoint)
-
-	token, err := uaaClient.FetchToken(true)
-	if err != nil{
-		return err
+	client := uaa.Client{
+		ClientID:             cc.config.clientIndentity,
+		ClientSecret:         cc.config.clientPwd,
+		AccessTokenValidity:  1209600,
+		AuthorizedGrantTypes: []string{"client_credentials", "refresh_token"},
+		Scope:                []string{"openid", "oauth.approvals", "credhub.read", "credhub.write"},
+		Authorities:          []string{"oauth.login", "credhub.read", "credhub.write"},
 	}
 
-	fmt.Printf("Response:\n\ttoken: %s\n\texpires: %d\n", token.AccessToken, token.ExpiresIn)
+	newClient, err := api.CreateClient(client)
 
+	logger.Info(newClient.DisplayName)
 
-	nc := &schema.OauthClient{
-		ClientId:     cc.clientIndentity,
-		ClientSecret: cc.clientPwd,
-		Name: cc.clientIndentity,
-		AuthorizedGrantTypes: []string{"refresh_token","client_credentials"},
-		Authorities: []string{"cloud_controller.admin_read_only"},
+	//api.ChangeClientSecret("credhub_admin_client", "iFB7oFXyRI1Yp3sHd_5RZ7WLDZHv2UX3")
+	credHubClient, err := api.GetClient("credhub_admin_client")
 
-	}
-	newClient, err := uaaClient.RegisterOauthClient(nc)
-	if err!= nil {
-		return err
-	}
+	chAdmin, err := credhub.New("https://credhub-proxy.apps.brea.cf-app.com",
+		credhub.SkipTLSValidation(true),
+		credhub.Auth(auth.UaaClientCredentials("credhub_admin_client", credHubClient.ClientSecret)),
+		credhub.AuthURL("https://uaa.sys.brea.cf-app.com"),
+	)
 
-	fmt.Printf("New client name is %s", newClient.Name)
+	_, err = chAdmin.AddPermission(
+		"/*", "uaa-client:"+cc.config.clientIndentity,
+		[]string{"read", "write", "delete", "read_acl", "write_acl"},
+	)
 
 	return nil
 }
@@ -117,5 +113,4 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-
 }
