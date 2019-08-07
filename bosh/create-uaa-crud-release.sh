39b94232d027486d87212225df9021b54ebc4446
aaronmeza <ameza@pivotal.io>
2019-08-07 15:01:03 -0500

    Added makefile skip failing unit tests

--- bosh/create-uaa-crud-release.sh
+++ bosh/create-uaa-crud-release.sh
@@ -3,11 +3,11 @@ set -ex
 
 bosh add-blob ../../uaa-crud.linux uaa-crud.linux
 
-bosh create-release --name=uaa-crud --force --tarball=../uaa-crud-release.tgz
+bosh create-release --name=uaa_crud --force --tarball=../uaa-crud-release.tgz
 
 bosh upload-release ../uaa-crud-release.tgz
 
 bosh upload-stemcell --sha1 712632e687388f335578956fceff27f0836646ae \
   https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-xenial-go_agent?v=456.3
 
-bosh deploy -d uaa-crud ../manifests/sample-manifest.yaml -l ../manifests/values.yaml --recreate
+bosh deploy -d uaa_crud ../manifests/sample-manifest.yaml -l ../manifests/values.yaml --recreate
